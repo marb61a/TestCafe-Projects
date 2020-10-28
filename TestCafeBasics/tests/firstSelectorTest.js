9b498176196a77c9090354474cc67788a205f54a
Martin <marb61a@gmail.com>
2020-10-28 19:41:03 +0000

    Updated test files

---
+++ TestCafeBasics/tests/firstSelectorTest.js
@@ -0,0 +1,16 @@
+import {Selector} from 'testcafe';
+
+const developerName = Selector('#developer-name');
+const osOption = Selector('#macos');
+const submitButton = Selector('#submit-button');
+
+fixture("First Fixture")
+    .page("https://devexpress.github.io/testcafe/example/")
+
+test("First Test", async t => {
+    await t
+        .typeText(developerName, "Martin")
+        .click(osOption)
+        .click(submitButton);
+
+});
