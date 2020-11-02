966d4bcdbd798c87337d09ee5b132cedebadb448
Martin <marb61a@gmail.com>
2020-11-02 20:43:34 +0000

    added dropdownlist test file and updated notes and iFrame test file

--- TestCafeBasics/tests/iFrameTest.js
+++ TestCafeBasics/tests/iFrameTest.js
@@ -1,7 +1,18 @@
 import {Selector} from 'testcafe';
 
-const iframe =  Selector('iframe#mce_0_ifr');
+const iframeName =  Selector('iframe#mce_0_ifr');
 const textArea = Selector('body#tinymce.mce-content-body');
 
 fixture("Iframe Fixture")
-    .page("https://the-internet.herokuapp.com/iframe")
+    .page("https://the-internet.herokuapp.com/iframe");
+    
+test("iFrame test", async t =>{
+    await t
+        .switchToIframe(iframeName)
+        .click(textArea)
+        .pressKey('ctrl+a+delete')
+        .typeText(textArea,'Hello from Martin')
+        .expect(textArea.innerText).contains('Martin')
+        .switchToMainWindow();
+
+});
