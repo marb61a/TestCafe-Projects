2251ca58377012411583b8fa1b2c9409e128d72a
Martin <marb61a@gmail.com>
2020-11-02 21:01:33 +0000

    added uploadFile and dropdownList test files

--- TestCafeBasics/tests/dropdownListTest.js
+++ TestCafeBasics/tests/dropdownListTest.js
@@ -2,3 +2,12 @@ import {Selector} from 'testcafe';
 
 const interfaceSelect = Selector('select#preferred-interface');
 const interfaceOptions = interfaceSelect.find('option');
+
+fixture("Select an element from a Dropdownlist")
+    .page("https://devexpress.github.io/testcafe/example/");
+
+test('select element test', async t =>{
+    await t
+    .click(interfaceSelect)
+    .click(interfaceOptions.withText('Both'));
+});
