8f359bebdef9bf0610ecd4a2e5366828afb72dd5
Martin <marb61a@gmail.com>
2020-11-05 20:04:43 +0000

    Added dragTest and HoverTest and updated notes

--- TestCafeBasics/tests/dragTest.js
+++ TestCafeBasics/tests/dragTest.js
@@ -5,3 +5,11 @@ const slider = Selector('#slider');
 
 fixture ('Drag Test')
     .page('https://devexpress.github.io/testcafe/example/');
+
+test('Drag testcase', async t => {
+    await t
+        .setTestSpeed(0.01)
+        .click(triedCheckbox)
+        .drag(slider, 360, 0, {offsetX:10, offsetY:10});
+        
+});
