f529a9de34425e16cefb32ed41ffb3b5a1d0713a
Martin <marb61a@gmail.com>
2020-11-11 20:58:54 +0000

    Updated notes file and added initial screenshot demo test

---
+++ TestCafeBasics/tests/screenshotDemo.js
@@ -0,0 +1,32 @@
+import {Selector} from 'testcafe';
+
+const developerName = Selector('#developer-name');
+const osOption = Selector('#macos');
+const submitButton = Selector('#submit-button');
+
+fixture("First Fixture")
+    .page("http://devexpress.github.io/testcafe/")
+    .beforeEach(async t => {
+        await t 
+            .setTestSpeed(0.1)
+            .setPageLoadTimeout(0);
+    });
+
+test
+    .page("https://devexpress.github.io/testcafe/example/")
+("First Test", async t => {
+    await t 
+    .typeText(developerName, "Martin")
+    .click(osOption)
+    .click(submitButton);
+
+});
+
+test.page("https://devexpress.github.io/testcafe/example/")
+("Second Test", async t => {
+    await t
+        .typeText("#developer-name", "Martin")
+        .click("#windows")
+        .click("#submit-button")
+
+});
