111ee7df0260efbff7eba4ee4d70e992478666c2
Martin <marb61a@gmail.com>
2020-11-15 19:46:33 +0000

    Added elementScreenshot

---
+++ TestCafeBasics/tests/elementScreenshotDemo.js
@@ -0,0 +1,45 @@
+import {Selector} from 'testcafe';
+
+const developerName = Selector('#developer-name');
+const osOption = Selector('#macos');
+const submitButton = Selector('#submit-button');
+
+fixture("First Fixture")
+    .page("http://devexpress.github.io/testcafe/")
+    .beforeEach(async t => {
+        await t 
+            .setTestSpeed(0.1)
+            .setPageLoadTimeout(0);
+    });
+
+test
+    .page("https://devexpress.github.io/testcafe/example/")
+    ("First Test", async t =>{
+        await t
+            .typeText("#developer-name","Martin")
+            .click("#macos")
+            .click("#submit-button")
+            .takeScreenshot();
+    });
+    
+test
+    .page("https://devexpress.github.io/testcafe/example/")
+    ("Second Test", async t =>{
+        await t
+            .typeText("#developer-name","Martin")
+            .takeElementScreenshot("#developer-name")
+            .click("#macos")
+            .takeScreenshot()
+            .click("#submit-button");
+    });
+    
+test
+    .page("https://devexpress.github.io/testcafe/example/")
+    ("Third Test", async t =>{
+        await t
+            .typeText("#developer-name","Martin")
+            .click("#macos")
+            .takeElementScreenshot("#submit-button")
+            .click("#submit-button")
+            .takeScreenshot();
+    });
