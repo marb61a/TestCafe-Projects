e65901d94b774b9de055b6aaa9a9e65fe30c831d
Martin <marb61a@gmail.com>
2020-11-16 12:22:45 +0000

    Added functionality to  HomePage page file

--- PageModelDemo/pages/HomePage.js
+++ PageModelDemo/pages/HomePage.js
@@ -1,6 +1,28 @@
 import { Selector,t } from 'testcafe';
 
 class HomePage{
+    constructor(){
+        this.subtitleHeader = Selector('h2')
+            .withText('Welcome to the store');
+        this.RegisterLink = Selector('a').withText('Register');
+        this.LoginLink = Selector('a').withText('Log in');
+        this.CartLink = Selector('a').withText('Shopping cart');
+        this.MyAccountLink = Selector('a').withText('My account');
+        this.LogoutLink = Selector('a').withText('Log out');
+        this.currencyList = Selector("select#customerCurrency");
+
+    }
+
+    getproductSearch(){
+        return Selector("input[id='small-searchterms']");
+    }
+
+    async search(){
+        await t
+            .typeText(this.productSearch, product)
+            .wait(3000)
+            .pressKey('enter');
+    }
 
 }
 
