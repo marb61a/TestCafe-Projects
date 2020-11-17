80a6a7089360f779cf5fda683bd88b811b4108d3
Martin <marb61a@gmail.com>
2020-11-17 12:04:29 +0000

    Updated Register Page

--- PageModelDemo/pages/RegisterPage.js
+++ PageModelDemo/pages/RegisterPage.js
@@ -11,7 +11,34 @@ class RegisterPage {
         this.DateOfBirthYearList = Selector("select[name='DateOfBirthYear']");        
 
         this.Email = Selector('#Email');
+        this.Password = Selector('#Password');
+        this.ConfirmPassword = Selector('#ConfirmPassword');
+        this.RegisterButton = Selector('#register-button.button-1.register-next-step-button');
+        this.SuccessfullMessage = Selector('div.result').withText('Your registration completed');
+
     }
+
+    async selectDay(day){
+        const DaysOption = this.DateOfBirthDayList.find('option');
+        await t
+            .click(this.DateOfBirthDayList)
+            .click(DaysOption.withText(day));
+    }
+
+    async selectMonth(month){
+        const DaysOption = this.DateOfBirthMonthList.find('option');
+        await t
+            .click(this.DateOfBirthMonthList)
+            .click(DaysOption.withText(month));
+    }
+
+    async selectYear(year){
+        const DaysOption = this.DateOfBirthYearList.find('option');
+        await t
+            .click(this.DateOfBirthYearList)
+            .click(DaysOption.withText(year));
+    }
+
 }
 
 export default RegisterPage;
