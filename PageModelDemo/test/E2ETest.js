292f23e8c1afa81e52d674e855acd5432f5079b1
Martin <marb61a@gmail.com>
2020-11-18 11:47:51 +0000

    Updated E2E test

--- PageModelDemo/test/E2ETest.js
+++ PageModelDemo/test/E2ETest.js
@@ -11,3 +11,29 @@ const URL = 'https://demo.nopcommerce.com/';
 const getURL = ClientFunction(() => window.location.href);
 var randomNumber = Math.floor(Math.random() * 10000);
 var userEmail = 'martin'+randomNumber+'@test.com';
+
+fixture ('E2E Fixture')
+    .page(URL);
+
+test("Assert Home Page", async t => {
+    await t
+        .expect(getURL()).eql(URL)
+        .takeScreenshot()
+        .expect(homepage.subtitleHeader.exists).ok();
+});
+
+test("Place order E2E test", async t => {
+    await t
+        .maximizeWindow()
+        .click(homepage.RegisterLink)
+        .expect(getURL()).contains('register')
+        .click(registerpage.GenderOption)
+        .typeText(registerpage.FirstName, 'Martin')
+        .typeText(registerpage.LastName, 'Byrne')
+        .typeText(registerpage.Email, userEmail)
+        .typeText(registerpage.Password, 'ABC123')
+        .typeText(registerpage.ConfirmPassword, 'ABC123')
+        .click(registerpage.RegisterButton)
+        .expect(registerpage.SuccessfullMessage.exists).ok();
+        
+});
