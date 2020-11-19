6bfcd4cd1b84c24f9ee0635af5ee4c3fe4a3223e
Martin <marb61a@gmail.com>
2020-11-19 20:11:56 +0000

    Updated setpdefs file

--- BDD-Demo/features/step_definitions/stepdefs.js
+++ BDD-Demo/features/step_definitions/stepdefs.js
@@ -9,35 +9,47 @@ Given('I open the registration page', async function () {
     await testController.navigateTo(URL);
 });
 
-When('I select the gender', function () {
-
+When('I select the gender', async function () {
+    await testController
+        .click(registerpage.RegisterPage.GenderButton());
 });
 
-When('I enter First Name {string}', function (string) {
-
+When('I enter First Name {string}', async function (firstname) {
+    await testController
+        .typeText(registerpage.RegisterPage.FirstName(), firstname);
 });
 
-When('I enter Last Name {string}', function (string) {
-
+When('I enter Last Name {string}', async function (lastname) {
+    await testController
+        .typeText(registerpage.RegisterPage.Lastname(),lastname);
 });
 
-When('I select Date of Birth {string}', function (string) {
-
+When('I select Date of Birth {string}', async function (day) {
+    await testController
+        .click(registerpage.RegisterPage.DateOfBirth());
+    await testController
+        .click(registerpage.RegisterPage.ListOption().withText(day));
 });
 
-When('I select Month of Birth {string}', function (string) {
-
+When('I select Month of Birth {string}', async function (month) {
+    await testController
+        .click(registerpage.RegisterPage.MonthOfBirth());
+    await testController
+        .click(registerpage.RegisterPage.ListOption().withText(month));
 });
 
-When('I select Year of Birth {string}', function (string) {
+When('I select Year of Birth {string}', async function (year) {
+    await testController
+        .click(registerpage.RegisterPage.YearOfBirth());
+    await testController
+        .click(registerpage.RegisterPage.ListOption().withText(year));    
+});
+
+When('I enter Email {string}', async function (email) {
 
 });
 
 When('I enter Password {string}', function (string) {
-
-});
-
-When('I enter Email {string}', function (string) {
 
 });
 
