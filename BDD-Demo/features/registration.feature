79121ea2160709251eb5b9fab6ad2ecc88c91316
Martin <marb61a@gmail.com>
2020-11-19 12:02:44 +0000

    Added stepdefs file

--- BDD-Demo/features/registration.feature
+++ BDD-Demo/features/registration.feature
@@ -7,3 +7,11 @@ Given I open the registration page
 When I select the gender
 And I enter First Name "Joe"
 And I enter Last Name "Bloggs"
+And I select Date of Birth "1"
+And I select Month of Birth "May"
+And I select Year of Birth "1980"
+And I enter Email "joebloggs@test.com"
+And I enter Password "ABC123"
+And I enter Confirm Password "ABC123"
+And I click register button
+Then successful message is displayed
---
+++ BDD-Demo/features/step_definitions/stepdefs.js
@@ -0,0 +1,33 @@
+const assert = require('assert');
+const { Given, When, Then } = require('cucumber');
+
+Given('I open the registration page', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('I select the gender', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('I enter First Name {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('I enter Last Name {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('I select Date of Birth {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('I select Month of Birth {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
