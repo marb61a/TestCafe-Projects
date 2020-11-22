80cf4656ef6e3453f33aff5c57e22ebfce931e8f
Martin <marb61a@gmail.com>
2020-11-22 11:35:37 +0000

    Updated testControllerHolder functions

--- BDD-Demo/features/support/testControllerHolder.js
+++ BDD-Demo/features/support/testControllerHolder.js
@@ -1,7 +1,27 @@
 const testControllerHolder =  {
     testController:null,
     captureResolver: null,
-    getResolver: null
+    getResolver: null,
+
+    capture: function(t){
+        testControllerHolder.testController = t;
+
+        if(testControllerHolder.getResolver){
+            testControllerHolder.getResolver(t);
+        }
+
+        return new Promise(function(resolve){
+            testControllerHolder.captureResolver = resolve;
+        });
+    },
+
+    free: function(){
+        testControllerHolder.testController = null;
+
+        if(testControllerHolder.captureResolver){
+            testControllerHolder.captureResolver();
+        }
+    }
 
 }
 
