f368ffb270b79e5bb543527473e42281b00b49ac
Martin <marb61a@gmail.com>
2020-11-22 12:10:49 +0000

    Updated world.js functions

--- BDD-Demo/features/support/world.js
+++ BDD-Demo/features/support/world.js
@@ -10,6 +10,32 @@ function CustomWorld({ attach, parameters }){
             return testController = tc;
         });
     this.attach = attach;
+
+    this.setBrowser = function(){
+        if(parameters.browser === undefined){
+            return 'chrome';
+        } else {
+            return parameters.browser;
+        }
+
+    },
+
+    this.addScreenshotToReport = function(){
+        if (process.argv.includes('--format') || process.argv.includes('-f') || process.argv.includes('--format-options')){
+            testController.takeScreenshot()
+                .then(function(screenshotPath){
+
+                })
+                .catch(function(error){
+                    console.warn('The screenshot was not attached to the report');
+                });
+
+        } else {
+
+        }
+
+    }
+
 }
 
 setWorldConstructor(CustomWorld);
