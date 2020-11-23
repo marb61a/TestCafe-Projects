005b337c1c13bf8e1f843df8e37a800d30ccc00a
Martin <marb61a@gmail.com>
2020-11-23 12:14:04 +0000

    Added testcaferc and test files

--- BDD-Demo/features/support/world.js
+++ BDD-Demo/features/support/world.js
@@ -2,7 +2,7 @@ const {setWorldConstructor} = require('cucumber');
 const base64Img = require('base64-img');
 
 const testControllerHolder = require('./testControllerHolder');
-const { testController } = require('./testControllerHolder');
+// const { testController } = require('./testControllerHolder');
 
 function CustomWorld({ attach, parameters }){
     this.waitForTestController = testControllerHolder.get()
