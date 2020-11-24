dd0b420b0518fad8ea253fdd0e7e3573e346ad74
Martin <marb61a@gmail.com>
2020-11-24 11:29:01 +0000

    Update reporter demo project folder

---
+++ ReporterDemo/pages/ProductDetailsPage.js
@@ -0,0 +1,13 @@
+import { Selector } from 'testcafe';
+
+class ProductDetailsPage{
+    constructor(){
+        this.productPrice = Selector("span[id='price-value-4']").withText('$1,800');
+        this.productQuantity = Selector("input#product_enteredQuantity_4.qty-input");
+        this.addToCart = Selector("input[id='add-to-cart-button-4']");
+        this.successMessage = Selector("div.bar-notification.success");
+    }
+
+}
+
+export default new ProductDetailsPage();
